3b56b961a45e1d9c60e91c852962d5bfd666fa84
Kendall McGree <mcgreekendall@gmail.com>
2022-10-16 17:27:44 -0500

    install styled-components dependency and implement Header's styled-component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,14 @@
 import logo from "../../images/smartTechLogo.jpg";
 import cart from "../../images/empty-cart.png";
+import { StyledHeader } from "../styles/Header.styled";
 
 const Header = () => {
   return (
-    <>
+    <StyledHeader>
       <img src={logo} />
       <h1>smart Tech</h1>
       <img src={cart} />
-    </>
+    </StyledHeader>
   );
 };
 
