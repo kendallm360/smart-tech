7655418d6f34ebcd0d251cfedfc8f4e655d8c49f
Kendall McGree <mcgreekendall@gmail.com>
2022-10-16 17:17:41 -0500

    add header images to images folder and imported them into the component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,12 @@
+import logo from "../../images/smartTechLogo.jpg";
+import cart from "../../images/empty-cart.png";
+
 const Header = () => {
   return (
     <>
+      <img src={logo} />
       <h1>smart Tech</h1>
+      <img src={cart} />
     </>
   );
 };
