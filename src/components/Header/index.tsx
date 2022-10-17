864df3a7ca2570139fbd1d265f75618aa9d928a2
Kendall McGree <mcgreekendall@gmail.com>
2022-10-17 13:23:51 -0500

    add tests for Header component, all pass

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,9 +6,14 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <Link to="/">
-      <StyledHeader>
-        <img className="logo" src={logo} alt="Smart Tech logo" />
-        <img className="cart" src={cart} alt="cart emoji" />
+      <StyledHeader data-cy="header">
+        <img
+          data-cy="smart-tech-logo"
+          className="logo"
+          src={logo}
+          alt="Smart Tech logo"
+        />
+        <img data-cy="cart-logo" className="cart" src={cart} alt="cart emoji" />
       </StyledHeader>
     </Link>
   );
