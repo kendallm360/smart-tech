96ef5d5a3093d2d96483d4caffcd912882d08059
Kendall McGree <mcgreekendall@gmail.com>
2022-10-17 13:58:52 -0500

    add utils file and div for categories

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,9 +1,13 @@
 import { StyledCategories } from "../styles/Categories.styled";
+
 const Categories = () => {
   return (
     <StyledCategories>
       <h2>Fall Sale</h2>
       <h3> Shop these deals and more by category:</h3>
+      <div className="categories">
+        <img src="" alt="Cell Phone" />
+      </div>
     </StyledCategories>
   );
 };
