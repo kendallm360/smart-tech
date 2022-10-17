f44062196b3b5d1a59b315ffae9c634a90d270cd
Kendall McGree <mcgreekendall@gmail.com>
2022-10-17 13:42:08 -0500

    add categories component to app container

---
+++ src/components/Categories/index.tsx
@@ -0,0 +1,9 @@
+const Categories = () => {
+  return (
+    <>
+      <h2>Fall Sale</h2>
+      <h3> Shop these deals and more by category:</h3>
+    </>
+  );
+};
+export default Categories;
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import Header from "../../components/Header";
 import "./App.css";
 import { Route } from "react-router-dom";
+import Categories from "../../components/Categories";
 
 const App = () => {
   return (
     <>
       <Header />
       <Route exact path="/">
-        {/* <div className="App">
-
-</div> */}
+        <div className="App">
+          <Categories />
+        </div>
       </Route>
     </>
   );
