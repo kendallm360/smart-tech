f1412ce74a05ef6c48efed057d6c09aebef24338
Kendall McGree <mcgreekendall@gmail.com>
2022-10-17 15:28:15 -0500

    display and style images in Categories component

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,4 +1,13 @@
 import { StyledCategories } from "../styles/Categories.styled";
+import cellphone from "../../images/iphone.jpeg";
+import desktop from "../../images/desktop1.jpeg";
+import digital_camera from "../../images/digital_camera.jpeg";
+import headphones from "../../images/headphones.jpeg";
+import laptop from "../../images/laptop.jpeg";
+import refrigerator from "../../images/refrigerator.jpeg";
+import tablet from "../../images/tablet.jpeg";
+import tv from "../../images/tv.jpeg";
+import washers from "../../images/washers.jpeg";
 
 const Categories = () => {
   return (
@@ -6,7 +15,45 @@ const Categories = () => {
       <h2>Fall Sale</h2>
       <h3> Shop these deals and more by category:</h3>
       <div className="categories">
-        <img src="" alt="Cell Phone" />
+        <div className="category-body">
+          <img className="category-images" src={cellphone} alt="Cell Phone" />
+        </div>
+        <div className="category-body">
+          <img className="category-images" src={desktop} alt="Desktop" />
+        </div>
+        <div className="category-body">
+          <img
+            className="category-images"
+            src={digital_camera}
+            alt="Digital Camera"
+          />
+        </div>
+        <div className="category-body">
+          <img className="category-images" src={headphones} alt="Headphones" />
+        </div>
+        <div className="category-body">
+          <img className="category-images" src={laptop} alt="Laptop" />
+        </div>
+        <div className="category-body">
+          <img
+            className="category-images"
+            src={refrigerator}
+            alt="Refrigerator"
+          />
+        </div>
+        <div className="category-body">
+          <img className="category-images" src={tablet} alt="Tablet" />
+        </div>
+        <div className="category-body">
+          <img className="category-images" src={tv} alt="Flat screen TV" />
+        </div>
+        <div className="category-body">
+          <img
+            className="category-images"
+            src={washers}
+            alt="Side by Side Washer and Dryer combo"
+          />
+        </div>
       </div>
     </StyledCategories>
   );
--- src/components/styles/Categories.styled.js
+++ src/components/styles/Categories.styled.js
@@ -1,5 +1,24 @@
 import styled from "styled-components";
 
 export const StyledCategories = styled.section`
-  border: solid 3px red;
+  border: solid 1px red;
+  height: 90vh;
+  width: 100vw;
+
+  .categories {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1em;
+  }
+
+  .category-images {
+    height: 5em;
+    width: 5em;
+  }
+
+  .category-body {
+    border: solid 0.1em #df0000;
+    border-radius: 100%;
+    padding: 1em;
+  }
 `;
