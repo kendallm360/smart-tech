bb0bf7e5e258cb3126886ae7419e1cdbbb36ec1b
Kendall McGree <mcgreekendall@gmail.com>
2022-10-19 19:26:18 -0500

    create styled component for Item and add initial styling

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { ICategory } from "../../utils/Types";
 import { findCategory } from "../../utils/utils";
+import { StyledItem } from "../styles/Item.styled";
 
 type TItem = {
   image: string;
@@ -21,17 +22,18 @@ const Item = ({ id, name }: ICategory): JSX.Element => {
     });
   }, []);
   return (
-    <>
+    <StyledItem>
       <img src={item.image} alt={name} />
       {/* Add scroll of alt images here*/}
 
       <h2>{name}</h2>
+      <h3>Product Description</h3>
       <p>{item.longDescription}</p>
-      <p>{item.sku}</p>
+      <p>SKU: {item.sku}</p>
       <h3>{item.regularPrice}</h3>
       <button>Add to Cart</button>
       {/* Add scroll of other items here */}
-    </>
+    </StyledItem>
   );
 };
 
---
+++ src/components/styles/Item.styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledItem = styled.section`
+  margin-top: 10vh;
+
+  img {
+    height: 30vh;
+    margin-left: 10vw;
+    width: 80vw;
+  }
+
+  h2,
+  h3 {
+    text-align: center;
+  }
+
+  button {
+    background-color: #df0000;
+    border: #df0000;
+    border-radius: 0.4em;
+    font-size: 100%;
+    height: 2.5em;
+    margin-left: 10vw;
+    width: 80vw;
+  }
+`;
