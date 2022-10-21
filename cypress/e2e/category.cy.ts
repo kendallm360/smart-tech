595f0b3c4a6bc241c83779a54e7eab52ee1d4e24
Kendall McGree <mcgreekendall@gmail.com>
2022-10-21 16:25:23 -0500

    add initial tests for Category component, all pass

--- cypress/e2e/category.cy.ts
+++ cypress/e2e/category.cy.ts
@@ -1,14 +1,28 @@
+import cypress from "cypress";
+
 describe("empty spec", () => {
   beforeEach(() => {
     cy.intercept(
       "GET",
-      "http://localhost:3000/"
-      // {fixture: ""},
+      "https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=bEisDAA5LpWenkpnbqEt7RmL&format=json",
+      { fixture: "categories" }
     );
-    cy.visit("http://localhost:3000/cellphones");
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="category"]').eq(4).click();
   });
 
-  it.skip("Should have a header that says cellphones", () => {
-    cy.get("h2").should("equal", "cellphones");
+  it("Should have a header that says laptops", () => {
+    cy.get('[data-cy="category-header"]').contains("Laptops");
   });
+
+  //should show an error page if not more than 10
+  it("Should have more than 10 items to choose from", () => {
+    cy.wait(4000);
+    cy.get('[data-cy="item-card"]').should("have.length.at.least", 10);
+  });
+
+  it("Should allow the user to click on an item to see more information about it", () => {
+    cy.get('[data-cy="item-card"]').eq(0).click();
+  });
+  // add testing coverage for sorting and fitering once that is implemented
 });
