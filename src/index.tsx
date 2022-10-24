4c3aebec73471923779968e82ca4b56b837d1b2a
Kendall McGree <mcgreekendall@gmail.com>
2022-10-24 18:00:31 -0500

    import and use Query Client in index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,18 @@ import "./index.css";
 import App from "./containers/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const client = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
