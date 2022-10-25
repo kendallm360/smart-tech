b1156b88dcfa323c9b2130d059462be24dd3d2d3
Kendall McGree <mcgreekendall@gmail.com>
2022-10-25 17:00:40 -0500

    add tests for loading page while data is being fetched, all pass

--- cypress/e2e/errors.cy.ts
+++ cypress/e2e/errors.cy.ts
@@ -2,12 +2,20 @@ describe("errors", () => {
   beforeEach(() => {
     cy.intercept(
       "GET",
-      "https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=bEisDAA5LpWenkpnbqEt7RmL&format=json",
+      "https://api.bestbuy.com/1/products((categoryPath.id=abcat0502000))?apiKey=bEisDAA5LpWenkpnbqEt7RmL&format=json",
       { fixture: "categories" }
     );
     cy.visit("http://localhost:3000/");
   });
-  // it("passes", () => {
-  //   cy.visit("https://example.cypress.io");
-  // });
+
+  it("should show a loading page while data is being fetched", () => {
+    cy.get('[data-cy="category"]').eq(2).click();
+    cy.get('[data-cy="loading"]').should("exist");
+  });
+
+  it("Should show a loading page while data is being fetched", () => {
+    cy.get('[data-cy="category"]').eq(3).click();
+    cy.get('[data-cy="item-info"]').eq(1).click();
+    cy.get('[data-cy="loading"]').should("exist");
+  });
 });
