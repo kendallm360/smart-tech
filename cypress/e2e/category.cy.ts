14f577abc726469d27c3a80a05569e53e8f9af50
Kendall McGree <mcgreekendall@gmail.com>
2022-10-26 21:50:37 -0500

    add test for sorting from low to high, passes

--- cypress/e2e/category.cy.ts
+++ cypress/e2e/category.cy.ts
@@ -1,3 +1,5 @@
+import cypress from "cypress";
+
 describe("category", () => {
   beforeEach(() => {
     cy.intercept(
@@ -27,6 +29,16 @@ describe("category", () => {
     cy.go("back").url().should("equal", "http://localhost:3000/");
     cy.go("forward");
     cy.url().should("equal", "http://localhost:3000/Laptops");
+  });
+
+  it.only("Should be able to sort items from lowest price to highest", () => {
+    cy.get("select").select("low");
+    cy.get('[data-cy="name"]')
+      .eq(0)
+      .contains(
+        "Super Sonic - 10.1” Touch 2in1 Intel CherryTrail Z8350 2GB 32GB - Silver tablet with Black case and keyboard"
+      );
+    cy.get('[data-cy="price"]').eq(0).contains(199.0);
   });
   // add testing coverage for sorting and fitering once that is implemented
 });
