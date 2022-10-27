9b1e86a287906ea181289db0d8cc5c64d9c0a1fb
Kendall McGree <mcgreekendall@gmail.com>
2022-10-26 22:00:36 -0500

    adjust beforeEach block in Error's spec to improve testing

--- cypress/e2e/errors.cy.ts
+++ cypress/e2e/errors.cy.ts
@@ -6,15 +6,14 @@ describe("errors", () => {
       { fixture: "categories" }
     );
     cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="category"]').eq(2).click();
   });
 
   it("Should show a loading page while category data is being fetched", () => {
-    cy.get('[data-cy="category"]').eq(2).click();
     cy.get('[data-cy="loading"]').should("exist");
   });
 
   it("Should show a loading page while item data is being fetched", () => {
-    cy.get('[data-cy="category"]').eq(3).click().wait(3000);
     cy.get('[data-cy="item-info"]').eq(0).click();
     cy.get('[data-cy="loading"]').should("exist");
   });
