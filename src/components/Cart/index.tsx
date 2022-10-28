501a559190dff1760a60f7849cfc660ee7c99e56
Kendall McGree <mcgreekendall@gmail.com>
2022-10-28 14:32:43 -0500

    add initial layout of Cart component

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,13 +1,27 @@
+// import { all } from "cypress/types/bluebird";
 import { useContext } from "react";
 import { myContext } from "../..";
 const Cart = () => {
   const { cart } = useContext(myContext);
 
-  let allCart = cart.map((e: any) => e);
-  console.log(allCart, "worky?");
+  let entireCart = cart.map((e: any) => {
+    return (
+      <div className="cart-item" data-cy="cart-item">
+        <img src={e.image} alt={e.name}></img>
+        <div data-cy="cart-item-info" className="cart-item-info">
+          <h3>{e.name}</h3>
+          <h3>{e.price}</h3>
+          <span>{e.quantity}</span>
+          <button>Remove Item</button>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <>
       <h2>Your Cart: </h2>
+      {entireCart}
       <button disabled>Checkout</button>
     </>
   );
