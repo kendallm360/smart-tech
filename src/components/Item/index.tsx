624f1f1c32985535db7ed1b5523785e9ef872bed
Kendall McGree <mcgreekendall@gmail.com>
2022-10-31 16:23:03 -0500

    update price styling in Item component

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -46,7 +46,7 @@ const Item = ({ id, name }: TItem): JSX.Element => {
           <h3>Product Description</h3>
           <p>{item.longDescription}</p>
           <p>SKU: {item.sku}</p>
-          <h3>{item.regularPrice}</h3>
+          <h3>${item.regularPrice}</h3>
           <button
             onClick={() => {
               cart.push({
