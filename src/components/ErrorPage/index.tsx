64e10cd3dbeb1530710de0f466b13c8dc7a9c1dc
Kendall McGree <mcgreekendall@gmail.com>
2022-10-31 12:01:31 -0500

    create ErrorPage component and set up intial steps

---
+++ src/components/ErrorPage/index.tsx
@@ -0,0 +1,11 @@
+export const ErrorPage = () => {
+  return (
+    <>
+      <h1>
+        We’re really sorry to be holding you up today! Please know our teams are
+        working hard to get everything up and running, and we will update you
+        with the latest information.
+      </h1>
+    </>
+  );
+};
