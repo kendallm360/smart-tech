afa6a81ffd6d49d128a5476198fd781bee3ceb96
Kendall McGree <mcgreekendall@gmail.com>
2022-10-31 21:06:46 -0500

    add styling to homepage views for large screens

--- src/components/styles/App.styled.js
+++ src/components/styles/App.styled.js
@@ -3,3 +3,10 @@ import styled from "styled-components";
 export const StyledApp = styled.section`
   font-family: "impact-regular", sans-serif;
 `;
+
+// Common breakpoints are
+// 320px — 480px for mobile devices,
+// 481px — 768px for iPads & tablets,
+// 769px — 1024px for small screens like laptop
+// 1025px — 1200px for large screens like Desktops
+// 1201px and above for extra large screens like TV.
--- src/components/styles/Categories.styled.js
+++ src/components/styles/Categories.styled.js
@@ -10,9 +10,6 @@ export const StyledCategories = styled.section`
     text-align: center;
   }
 
-  h3 {
-  }
-
   .categories {
     display: grid;
     margin: 2%;
@@ -35,5 +32,64 @@ export const StyledCategories = styled.section`
   .category-label {
     text-align: center;
     color: #df0000;
+  }
+
+  @media screen and (min-width: 1025px) {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+
+    h2 {
+      font-size: 3em;
+      text-align: center;
+    }
+
+    h3 {
+      font-size: 1.5em;
+      margin-left: 3.5%;
+    }
+
+    p {
+      font-size: 1.5em;
+    }
+
+    .category {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      align-content: center;
+    }
+    .categories {
+      display: grid;
+      /* margin: 2%; */
+      grid-template-columns: repeat(4, 1fr);
+      grid-gap: 2em;
+    }
+
+    .category-images {
+      height: 8em;
+      padding: 0.1em;
+      width: 8em;
+    }
+
+    .category-body {
+      border: solid 0.1em #df0000;
+      height: 10em;
+      width: 10em;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      align-content: center;
+      /* padding: 1em; */
+    }
+
+    /*
+  .category-label {
+    text-align: center;
+    color: #df0000;
+  } */
   }
 `;
