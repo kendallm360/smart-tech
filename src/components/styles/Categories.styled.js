2c20584f5f06ec6333dac363ac875e88baaac27e
Kendall McGree <mcgreekendall@gmail.com>
2022-11-02 16:14:40 -0500

    correct order of operations for Categories' scoped styled components

--- src/components/styles/Categories.styled.js
+++ src/components/styles/Categories.styled.js
@@ -8,43 +8,47 @@ export const StyledCategories = styled.section`
 
   .header {
     text-align: center;
-    @media screen and (max-width: 480px) {
-      font-size: 2em;
-    }
-    @media screen and (max-width: 768px) {
-      font-size: 2.8em;
-    }
-    @media screen and (max-width: 1024px) {
-      font-size: 2.8em;
-    }
     @media screen and (min-width: 1025px) {
       font-size: 2.8em;
       margin-top: 1%;
     }
+    @media screen and (max-width: 1024px) {
+      font-size: 2.8em;
+    }
+    @media screen and (max-width: 768px) {
+      font-size: 2.8em;
+    }
+    @media screen and (max-width: 480px) {
+      font-size: 2em;
+    }
   }
 
   .sub-header {
-    @media screen and (max-width: 480px) {
-      font-size: 1.1em;
-      margin-bottom: 5%;
+    @media screen and (min-width: 1025px) {
+      font-size: 1.4em;
       margin-top: -2%;
-    }
-    @media screen and (max-width: 768px) {
-      font-size: 1.1em;
     }
     @media screen and (max-width: 1024px) {
       font-size: 1.4em;
       margin-top: -2%;
     }
-    @media screen and (min-width: 1025px) {
-      font-size: 1.4em;
+    @media screen and (max-width: 768px) {
+      font-size: 1.1em;
+    }
+    @media screen and (max-width: 480px) {
+      font-size: 1.1em;
+      margin-bottom: 5%;
       margin-top: -2%;
     }
   }
 
   .categories {
     display: grid;
-    @media screen and (max-width: 480px) {
+    @media screen and (min-width: 1025px) {
+      grid-template-columns: repeat(4, 1fr);
+      margin: 1%;
+    }
+    @media screen and (max-width: 1024px) {
       grid-gap: 1em;
       grid-template-columns: repeat(3, 1fr);
       margin: 1%;
@@ -54,13 +58,9 @@ export const StyledCategories = styled.section`
       grid-template-columns: repeat(3, 1fr);
       margin: 1%;
     }
-    @media screen and (max-width: 1024px) {
+    @media screen and (max-width: 480px) {
       grid-gap: 1em;
       grid-template-columns: repeat(3, 1fr);
-      margin: 1%;
-    }
-    @media screen and (min-width: 1025px) {
-      grid-template-columns: repeat(4, 1fr);
       margin: 1%;
     }
   }
@@ -75,61 +75,62 @@ export const StyledCategories = styled.section`
   .category-body {
     border: solid 0.1em #df0000;
     border-radius: 50%;
+    cursor: pointer;
     display: flex;
     justify-content: center;
     align-content: center;
     align-items: center;
-    @media screen and (max-width: 480px) {
-      height: 6em;
-      width: 6em;
-    }
-    @media screen and (max-width: 768px) {
-      height: 9em;
-      width: 9em;
+    @media screen and (min-width: 1025px) {
+      height: 11em;
+      width: 11em;
     }
     @media screen and (max-width: 1024px) {
       height: 10em;
       width: 10em;
     }
-    @media screen and (min-width: 1025px) {
-      height: 11em;
-      width: 11em;
+    @media screen and (max-width: 768px) {
+      height: 9em;
+      width: 9em;
+    }
+    @media screen and (max-width: 480px) {
+      height: 6em;
+      width: 6em;
     }
   }
 
   .category-images {
-    @media screen and (max-width: 480px) {
-      height: 4em;
-      width: 4em;
-    }
-    @media screen and (max-width: 768px) {
-      height: 6em;
-      width: 6em;
+    @media screen and (min-width: 1025px) {
+      height: 7.5em;
+      width: 7.5em;
     }
     @media screen and (max-width: 1024px) {
       height: 7em;
       width: 7em;
     }
-    @media screen and (min-width: 1025px) {
-      height: 7.5em;
-      width: 7.5em;
+    @media screen and (max-width: 768px) {
+      height: 6em;
+      width: 6em;
+    }
+    @media screen and (max-width: 480px) {
+      height: 4em;
+      width: 4em;
     }
   }
 
   .category-label {
     color: #df0000;
     text-align: center;
-    @media screen and (max-width: 480px) {
-      font-size: 1em;
-    }
-    @media screen and (max-width: 768px) {
-      font-size: 1em;
+    @media screen and (min-width: 1025px) {
+      font-size: 1.4em;
     }
     @media screen and (max-width: 1024px) {
       font-size: 1.4em;
     }
-    @media screen and (min-width: 1025px) {
-      font-size: 1.4em;
+    @media screen and (max-width: 768px) {
+      font-size: 1em;
+    }
+    @media screen and (max-width: 480px) {
+      font-size: 1em;
     }
   }
   .washers {
