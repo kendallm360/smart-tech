af4c0903b9fc9dd36f94decc6e512bed4a6dfece
Kendall McGree <mcgreekendall@gmail.com>
2022-11-03 16:24:18 -0500

    remove dead useContext code from root index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,16 @@
-import { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./containers/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import CartContext from "./contexts/context";
-
-// interface ICartItem {
-//   id: number;
-//   name: string;
-//   image: string;
-//   price: number;
-//   quantity: number;
-// }
-
-// interface AppContextInterface {
-//   cart: ICartItem[];
-// }
-
-//alterate logic for the line above myContext
-// const defaultContext = {
-//   cart: [
-//     //in the event I wanted cart to have at least 1 object inside with given structure
-//     // {
-//     //   name: "",
-//     //   image: "",
-//     //   price: 0,
-//     //   quantity: 0,
-//     // },
-//   ],
-// };
-//old example:
-// export const myContext = createContext<AppContextInterface | null>(null);
-//current variable in use:
-// export const myContext = createContext<AppContextInterface>(defaultContext);
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
     <CartContext>
-      {/* <myContext.Provider value={defaultContext}> */}
       <App />
-      {/* </myContext.Provider> */}
     </CartContext>
   </BrowserRouter>
 );
