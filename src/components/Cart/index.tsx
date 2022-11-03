119efa30f3483daede52a9ebdc095590b97ed7db
Kendall McGree <mcgreekendall@gmail.com>
2022-11-03 16:39:57 -0500

    add ability for user to remove items from their cart

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -2,7 +2,12 @@ import { useContext } from "react";
 import { AppContextInterface, CartContext } from "../../contexts/context";
 import { StyledCart } from "../styles/Cart.styled";
 const Cart = () => {
-  const { cart } = useContext<AppContextInterface>(CartContext);
+  const { cart, setCart } = useContext<AppContextInterface>(CartContext);
+
+  const handleRemoveFromCart = (event: any) => {
+    let sku = event.target.value;
+    setCart(cart.filter((item: any) => item.id !== parseInt(sku)));
+  };
 
   let entireCart = cart.map((e: any) => {
     return (
@@ -12,7 +17,13 @@ const Cart = () => {
           <h3 className="item-title">{e.name}</h3>
           <h3 className="item-price">${e.price.toFixed(2)}</h3>
           <span>{e.quantity}</span>
-          <button className="remove-item-button">Remove Item</button>
+          <button
+            value={e.id}
+            className="remove-item-button"
+            onClick={handleRemoveFromCart}
+          >
+            Remove Item
+          </button>
         </div>
       </div>
     );
