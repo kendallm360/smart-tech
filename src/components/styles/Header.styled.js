18b159559926cc8741ad7fb40911c6b6fa5ef003
Kendall McGree <mcgreekendall@gmail.com>
2022-11-02 22:25:55 -0500

    complete styling for Header component on all views

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -2,59 +2,82 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.header`
   background-color: #df0000;
-  cursor: pointer;
-  height: 10vh;
   width: 100vw;
-
-  span {
-    background-color: #facb35;
-    border: solid 0.1em black;
-    border-radius: 50%;
-    color: #df0000;
-    /* margin-top: 10%; */
-    padding: 1%;
-    vertical-align: 280%;
+  display: flex;
+  justify-content: space-between;
+  /* Desktops */
+  @media screen and (min-width: 1025px) {
+    height: 11vh;
+  }
+  /* Laptops */
+  @media screen and (max-width: 1024px) {
+    height: 11vh;
+  }
+  /* Tablets */
+  @media screen and (max-width: 768px) {
+    height: 10vh;
+  }
+  /* Mobile */
+  @media screen and (max-width: 480px) {
+    height: 10vh;
   }
 
   .logo {
-    height: 100%;
-    margin-right: 34%;
-    width: 40%;
+    cursor: pointer;
+    width: 40vw;
+    /* Desktops */
+    @media screen and (min-width: 1025px) {
+      height: 11vh;
+    }
+    /* Laptops */
+    @media screen and (max-width: 1024px) {
+      height: 11vh;
+    }
+    /* Tablets */
+    @media screen and (max-width: 768px) {
+      height: 10vh;
+    }
+    /* Mobile */
+    @media screen and (max-width: 480px) {
+      height: 10vh;
+    }
+  }
+
+  .cart-body {
+    width: 20vw;
+    @media screen and (min-width: 1025px) {
+      height: 11vh;
+    }
+    /* Laptops */
+    @media screen and (max-width: 1024px) {
+      height: 11vh;
+    }
+    /* Tablets */
+    @media screen and (max-width: 768px) {
+      height: 10vh;
+    }
+    /* Mobile */
+    @media screen and (max-width: 480px) {
+      height: 10vh;
+    }
   }
 
   .cart-logo {
     align-self: center;
+    cursor: pointer;
     height: 60%;
-    margin-bottom: 3%;
-    width: 20%;
+    margin-top: 15%;
+    position: relative;
+    width: 70%;
   }
 
-  //Desktops
-  @media screen and (min-width: 1025px) {
-    height: 12vh;
-    justify-content: space-between;
-
-    span {
-      background-color: #facb35;
-      border: solid 0.1em black;
-      border-radius: 50%;
-      color: #df0000;
-      height: 3.5em;
-      padding: 1%;
-      width: 3.5em;
-      vertical-align: 400%;
-    }
-
-    .logo {
-      height: 100%;
-      width: 40%;
-    }
-
-    .cart-logo {
-      cursor: pointer;
-      height: 60%;
-      margin-bottom: 3%;
-      width: 15%;
-    }
+  .cart-quantity {
+    background-color: #facb35;
+    border: solid 0.15em black;
+    border-radius: 50%;
+    color: #df0000;
+    cursor: pointer;
+    padding: 0.5%;
+    position: absolute;
   }
 `;
