056d0167a0d2a90b6f2cd372f11d2ba2d7c72dc6
Kendall McGree <mcgreekendall@gmail.com>
2022-11-07 19:36:14 -0600

    uncomment all test, all pass

--- cypress/e2e/cart.cy.ts
+++ cypress/e2e/cart.cy.ts
@@ -16,41 +16,41 @@ describe("cart", () => {
     cy.wait(6000);
   });
 
-  // it("Should show cart's header", () => {
-  //   cy.get('[data-cy="cart-header"]').should("exist");
-  // });
+  it("Should show cart's header", () => {
+    cy.get('[data-cy="cart-header"]').should("exist");
+  });
 
-  // it("Should have 2 elements in cart ", () => {
-  //   cy.get('[data-cy="cart-item"]').should("have.length", 2);
-  //   cy.get('[data-cy="cart-quantity"]').should("contain", 2);
-  // });
+  it("Should have 2 elements in cart ", () => {
+    cy.get('[data-cy="cart-item"]').should("have.length", 2);
+    cy.get('[data-cy="cart-quantity"]').should("contain", 2);
+  });
 
-  // it("Should allow the user to delete items from the cart", () => {
-  //   cy.get('[data-cy="remove-item-button"]').eq(0).click();
-  //   cy.get('[data-cy="cart-quantity"]').should("contain", 1);
-  // });
+  it("Should allow the user to delete items from the cart", () => {
+    cy.get('[data-cy="remove-item-button"]').eq(0).click();
+    cy.get('[data-cy="cart-quantity"]').should("contain", 1);
+  });
 
-  // it("Should not allow user to checkout", () => {
-  //   cy.get(".checkout-button").should("be.disabled");
-  // });
+  it("Should not allow user to checkout", () => {
+    cy.get(".checkout-button").should("be.disabled");
+  });
 
-  // it("Should show an error message if the cart is empty", () => {
-  //   cy.get('[data-cy="remove-item-button"]').eq(0).click();
-  //   cy.get('[data-cy="remove-item-button"]').eq(0).click();
-  //   cy.get('[data-cy="empty-cart-header"]').should(
-  //     "contain",
-  //     "Your Cart is Currently empty"
-  //   );
-  //   cy.get('[data-cy="cart-quantity"]').should("contain", 0);
-  //   cy.get('[data-cy="empty-cart-message"]').should(
-  //     "contain",
-  //     "Before you are able to 'checkout' you must add some products to your shopping cart"
-  //   );
-  // });
+  it("Should show an error message if the cart is empty", () => {
+    cy.get('[data-cy="remove-item-button"]').eq(0).click();
+    cy.get('[data-cy="remove-item-button"]').eq(0).click();
+    cy.get('[data-cy="empty-cart-header"]').should(
+      "contain",
+      "Your Cart is Currently empty"
+    );
+    cy.get('[data-cy="cart-quantity"]').should("contain", 0);
+    cy.get('[data-cy="empty-cart-message"]').should(
+      "contain",
+      "Before you are able to 'checkout' you must add some products to your shopping cart"
+    );
+  });
 
-  // it("Should allow user to update a single item's quantity", () => {
-  //   cy.get('[data-cy="quantity-dropdown"]').eq(0).select(3);
-  //   cy.get('[data-cy="quantity-dropdown"]').eq(0).should("have.value", 4);
-  // });
-  //test for router functionality as well
+  it("Should allow user to update a single item's quantity", () => {
+    cy.get('[data-cy="quantity-dropdown"]').eq(0).select(3);
+    cy.get('[data-cy="quantity-dropdown"]').eq(0).should("have.value", 4);
+  });
+  // test for router functionality as well
 });
