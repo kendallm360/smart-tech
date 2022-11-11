f06a978354f40b27c5e4905afae2b0a5bab34f30
Kendall McGree <mcgreekendall@gmail.com>
2022-11-10 18:52:41 -0600

    fix bug that leaked broken items into the Category component's list of items

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -93,60 +93,62 @@ const Category = ({ id }: ICategory): JSX.Element => {
     }
   };
 
-  let allItems = itemList.map((item: Item) => {
-    return (
-      <div
-        key={item.sku}
-        id={item.regularPrice}
-        data-cy="item-card"
-        className="item-card"
-      >
-        <div className="image-third">
-          <Link to={`/${id}/${item.name}`} style={linkStyle}>
-            <img className="item-image" src={item.image} alt={item.name} />
-          </Link>
-        </div>
-        <div className="item-info">
-          <Link to={`/${id}/${item.name}`} style={linkStyle}>
-            <h2 className="item-name" data-cy="item-name">
-              {item.name}
-            </h2>
-          </Link>
-          <div className="additional-item-info">
+  let allItems = itemList
+    .filter((item: Item) => !item.name.includes("/"))
+    .map((item: Item) => {
+      return (
+        <div
+          key={item.sku}
+          id={item.regularPrice}
+          data-cy="item-card"
+          className="item-card"
+        >
+          <div className="image-third">
             <Link to={`/${id}/${item.name}`} style={linkStyle}>
-              <h3 className="item-label">
-                Model:<span className="item-span"> {item.modelNumber}</span>{" "}
-              </h3>
+              <img className="item-image" src={item.image} alt={item.name} />
             </Link>
-            {/* <Link to={`/${id}/${item.name}`} style={linkStyle}>
+          </div>
+          <div className="item-info">
+            <Link to={`/${id}/${item.name}`} style={linkStyle}>
+              <h2 className="item-name" data-cy="item-name">
+                {item.name}
+              </h2>
+            </Link>
+            <div className="additional-item-info">
+              <Link to={`/${id}/${item.name}`} style={linkStyle}>
+                <h3 className="item-label">
+                  Model:<span className="item-span"> {item.modelNumber}</span>{" "}
+                </h3>
+              </Link>
+              {/* <Link to={`/${id}/${item.name}`} style={linkStyle}>
               <h3 className="item-label">
                 Release Date:
                 <span className="item-span"> {item.startDate}</span>{" "}
               </h3>
             </Link> */}
-            <Link to={`/${id}/${item.name}`} style={linkStyle}>
-              <h3 className="item-label">
-                SKU:<span className="item-span"> {item.sku}</span>{" "}
-              </h3>
-            </Link>
+              <Link to={`/${id}/${item.name}`} style={linkStyle}>
+                <h3 className="item-label">
+                  SKU:<span className="item-span"> {item.sku}</span>{" "}
+                </h3>
+              </Link>
+            </div>
+          </div>
+          <div className="price-cart">
+            <h3 className="item-price" data-cy="price">
+              {currencyFormatter.format(parseInt(item.regularPrice))}
+            </h3>
+            <button
+              data-cy="add-to-cart-button"
+              value={item.sku}
+              className={`cart-button ${item.sku}`}
+              onClick={handleAddToCart}
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
-        <div className="price-cart">
-          <h3 className="item-price" data-cy="price">
-            {currencyFormatter.format(parseInt(item.regularPrice))}
-          </h3>
-          <button
-            data-cy="add-to-cart-button"
-            value={item.sku}
-            className={`cart-button ${item.sku}`}
-            onClick={handleAddToCart}
-          >
-            Add to Cart
-          </button>
-        </div>
-      </div>
-    );
-  });
+      );
+    });
   return (
     <>
       {itemList.length === 0 ? (
