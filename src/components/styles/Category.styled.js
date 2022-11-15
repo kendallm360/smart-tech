8da023271bf302a6ae1be6412bbef5c631e54a1d
Kendall McGree <mcgreekendall@gmail.com>
2022-11-15 12:45:02 -0600

    fill in blank width spacing in Category component

--- src/components/styles/Category.styled.js
+++ src/components/styles/Category.styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledCategory = styled.section`
   background-color: #df0000;
   color: black;
+  width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
