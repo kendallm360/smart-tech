013cb5bf7372819fad0ed17915319c144f09240e
Kendall McGree <mcgreekendall@gmail.com>
2022-11-21 16:37:38 -0600

    remove dead code from apiCalls

--- src/utils/apicalls.tsx
+++ src/utils/apicalls.tsx
@@ -1,14 +1,3 @@
-//async await
-// const fetchCategory = (category: string) => {
-//   return fetch(`https://api.bestbuy.com/v1/products((categoryPath.id=${category}))?apiKey=bEisDAA5LpWenkpnbqEt7RmL&pageSize=50&format=json
-// `).then((response) => {
-//     if (response.ok) {
-//       return response.json();
-//     }
-//     throw Error(response.status.toString());
-//   });
-// };
-
 const fetchCategory = async (category: string) => {
   try {
     const response =
